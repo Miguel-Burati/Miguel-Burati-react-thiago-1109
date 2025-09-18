35351abd6017a39b9749373609c22f1c11517938
Miguel-Burati <269341@unifio.edu.br>
2025-09-18 14:32:21 +0000

    navegacao entre telas

---
+++ aula0911/Tela1.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Tela1() {
+    return(
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Tela 1</Text>
+        </View>
+    );
+}
