a08085b47529b8567307c46c2d0f4f9d9ad9a194
Miguel-Burati <269341@unifio.edu.br>
2025-09-25 01:16:25 +0000

    nav com drawer

---
+++ navegacao2/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Tela1 from './Tela1';
+import Tela2 from './Tela2';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name='Home' component={Tela1}/>
+        <Drawer.Screen name='Perfil' component={Tela2}/>
+      </Drawer.Navigator >
+    </NavigationContainer>
+  );
+}
+
---
+++ navegacao2/Tela2.jsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Tela2() {
+    return(
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Perfil</Text>
+        </View>
+    );
+}
